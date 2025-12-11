278f455cc3326ed9147c28610b104df83340e45e
fahadjan2 <fahadfawad2006@gmail.com>
2025-12-10 22:49:19 -0800

    Milestone 2

--- code.cpp
+++ code.cpp
@@ -36,33 +36,57 @@ int main() {
         newOrder->name = names[randNum];
         randNum = rand() % drinks.size();
         newOrder->drink = drinks[randNum];
-        if (!head) 
+        if (!head) //If head is empty
             head = newOrder; 
-        else {
-            while (current)
-            head->next = newOrder;
+        else { //Connects tail to neworder
+            Coffee* current = head;
+            while (current->next) {
+                current = current->next;
+            }
+            current->next = newOrder;
         }
         newOrder->next = nullptr;
     }
 
     //10 Simulation Rounds
     for (int i = 0; i < ROUNDS; i++) {
-        if (head) {
+        //Output
+        Coffee* current = head;
+        cout << "Coffee queue:" << endl;
+        while (current) {
+            cout << current->name << ": " << current->drink << endl; 
+            current = current->next;
+        }
+        cout << endl;
+
+        cout << "Round " << i + 1 << endl;
+        if (head) { //Serves the head of the line
             cout << head->name << " has been served " << head->drink << endl;
             Coffee* temp = head;
             head = temp->next;
-             
+            delete temp;
+        } else {
+            cout << "Queue empty" << endl;
         }
         int probability = rand() % 100 + 1;
         if (probability > 50) { //Joins queue
-            Coffee* current = nullptr;
             Coffee* newOrder = new Coffee;
             int randNum = rand() % names.size();
             newOrder->name = names[randNum];
             randNum = rand() % drinks.size();
             newOrder->drink = drinks[randNum];
+            if (!head) //If head is empty
+                head = newOrder; 
+            else { //Connects tail to neworder
+                Coffee* current = head;
+                while (current->next) {
+                    current = current->next;
+                }
+                current->next = newOrder;
+            }
             newOrder->next = nullptr;
-            head = newOrder;
+
+            cout << newOrder->name << " has joined the queue" << endl;
         }
     }
 
