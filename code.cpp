d6da407ecc8ef92355e8bb69ebc700ed335cea0a
fahadjan2 <fahadfawad2006@gmail.com>
2025-12-10 23:04:32 -0800

    Commit

--- code.cpp
+++ code.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <iomanip>
 #include <vector>
+#include <deque>
 using namespace std;
 
 const int ROUNDS = 10;
@@ -11,6 +12,9 @@ struct Coffee {
     string drink;
     Coffee* next;
 };
+
+void coffeeOutput(Coffee *);
+void addCoffeeOrder(Coffee *&, string, string);
 
 int main() { 
     srand(time(0));
@@ -31,63 +35,71 @@ int main() {
     Coffee* head = nullptr;
     //Initializing 3 customers
     for (int i = 0; i < 3; i++) {
-        Coffee* newOrder = new Coffee;
         int randNum = rand() % names.size();
-        newOrder->name = names[randNum];
+        string name = names[randNum];
         randNum = rand() % drinks.size();
-        newOrder->drink = drinks[randNum];
-        if (!head) //If head is empty
-            head = newOrder; 
-        else { //Connects tail to neworder
-            Coffee* current = head;
-            while (current->next) {
-                current = current->next;
-            }
-            current->next = newOrder;
-        }
-        newOrder->next = nullptr;
+        string drink = drinks[randNum];
+        addCoffeeOrder(head, name, drink);
     }
+
+    cout << "Initial queue:" << endl;
+    coffeeOutput(head);
 
     //10 Simulation Rounds
     for (int i = 0; i < ROUNDS; i++) {
-        //Output
-        Coffee* current = head;
-        cout << "Coffee queue:" << endl;
-        while (current) {
-            cout << current->name << ": " << current->drink << endl; 
-            current = current->next;
-        }
-        cout << endl;
-
         cout << "Round " << i + 1 << endl;
+
+        //COFFEE LINE
         if (head) { //Serves the head of the line
             cout << head->name << " has been served " << head->drink << endl;
             Coffee* temp = head;
             head = temp->next;
             delete temp;
-        } else {
-            cout << "Queue empty" << endl;
         }
+
         int probability = rand() % 100 + 1;
         if (probability > 50) { //Joins queue
-            Coffee* newOrder = new Coffee;
             int randNum = rand() % names.size();
-            newOrder->name = names[randNum];
+            string name = names[randNum];
             randNum = rand() % drinks.size();
-            newOrder->drink = drinks[randNum];
-            if (!head) //If head is empty
-                head = newOrder; 
-            else { //Connects tail to neworder
-                Coffee* current = head;
-                while (current->next) {
-                    current = current->next;
-                }
-                current->next = newOrder;
-            }
-            newOrder->next = nullptr;
-
-            cout << newOrder->name << " has joined the queue" << endl;
+            string drink = drinks[randNum];
+            addCoffeeOrder(head, name, drink);
+            cout << name << " has joined the queue" << endl;
         }
+
+        //Outputs
+        coffeeOutput(head);
     }
 
 }   
+
+void coffeeOutput(Coffee* head) {
+    if (!head) {
+        cout << "Queue empty" << endl << endl;
+        return;
+    }
+    
+    Coffee* current = head;
+    cout << "Coffee queue:" << endl;
+    while (current) {
+        cout << current->name << ": " << current->drink << endl; 
+        current = current->next;
+    }
+    cout << endl;
+}
+
+void addCoffeeOrder(Coffee*& head, string name, string drink) {
+    Coffee* newOrder = new Coffee;
+    newOrder->name = name;
+    newOrder->drink = drink;
+    newOrder->next = nullptr;
+    if (!head) //If head is empty
+        head = newOrder; 
+    else { //Connects tail to neworder
+        Coffee* current = head;
+        while (current->next) {
+            current = current->next;
+        }
+        current->next = newOrder;
+    }
+}
