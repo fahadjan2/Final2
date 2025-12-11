721bf9ec579fb8262ffc995a011bdbb3fc499699
fahadjan2 <fahadfawad2006@gmail.com>
2025-12-10 22:03:01 -0800

    Commit

--- code.cpp
+++ code.cpp
@@ -2,6 +2,18 @@
 
 using namespace std;
 
+struct Node {
+    string name;
+    string drink;
+    Node* next;
+    
+    Node(string nm, string dr) {
+        name = nm;
+        drink = dr;
+        next = nullptr;
+    }
+};
+
 int main() {
     
 }
