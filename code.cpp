9b79f0219f1a83d7e1499d87e40d7805a46ee348
fahadjan2 <fahadfawad2006@gmail.com>
2025-12-10 22:35:26 -0800

    Commit

--- code.cpp
+++ code.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <iomanip>
 #include <vector>
 using namespace std;
 
@@ -12,6 +13,8 @@ struct Coffee {
 };
 
 int main() { 
+    srand(time(0));
+
     //File populating w/ names and drinks for Coffee
     ifstream namesfile("names.txt");
     ifstream drinksfile("drinks.txt");
@@ -28,12 +31,39 @@ int main() {
     Coffee* head = nullptr;
     //Initializing 3 customers
     for (int i = 0; i < 3; i++) {
-        
+        Coffee* newOrder = new Coffee;
         int randNum = rand() % names.size();
+        newOrder->name = names[randNum];
         randNum = rand() % drinks.size();
+        newOrder->drink = drinks[randNum];
+        if (!head) 
+            head = newOrder; 
+        else {
+            while (current)
+            head->next = newOrder;
+        }
+        newOrder->next = nullptr;
     }
-    for (int i = 0; i < ROUNDS; i++) {
 
+    //10 Simulation Rounds
+    for (int i = 0; i < ROUNDS; i++) {
+        if (head) {
+            cout << head->name << " has been served " << head->drink << endl;
+            Coffee* temp = head;
+            head = temp->next;
+             
+        }
+        int probability = rand() % 100 + 1;
+        if (probability > 50) { //Joins queue
+            Coffee* current = nullptr;
+            Coffee* newOrder = new Coffee;
+            int randNum = rand() % names.size();
+            newOrder->name = names[randNum];
+            randNum = rand() % drinks.size();
+            newOrder->drink = drinks[randNum];
+            newOrder->next = nullptr;
+            head = newOrder;
+        }
     }
 
 }   
